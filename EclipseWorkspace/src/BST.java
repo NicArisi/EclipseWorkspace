996fbb544792f8339aff46710089b058530c0c8c
Nicolas F. Arisi Fernandez <nicoarisi339@gmail.com>
2023-11-28 10:34:01 -0500

    find method

--- EclipseWorkspace/src/BST.java
+++ EclipseWorkspace/src/BST.java
@@ -28,9 +28,44 @@ public class BST {
 			}
 		} while (!finished);
 	}
-
+	
 	BstNode rootNode;
-
+	public boolean Find(int num) {
+		BstNode currentNode = rootNode;
+		boolean finished = false;
+		boolean returner=false;
+		if(currentNode.intData==num) {
+			returner=true;
+		}else {
+			do {
+				BstNode curLeftNode = currentNode.leftNode;
+				BstNode curRightNode = currentNode.rightNode;
+				int curIntData = currentNode.intData;
+				if(curLeftNode ==null && curRightNode==null) {
+					finished=true;
+					returner=false;
+				}else {
+				if (num > curIntData) {
+					if (curRightNode.intData == num) {
+						returner=true;
+						finished = true;
+						break;
+					} else {
+						currentNode = currentNode.rightNode;
+					}
+				} else {
+					if (curLeftNode.intData == num) { // create a new node referenced with currentNode.leftNode
+						returner=true;
+						finished = true;
+					} else {
+						currentNode = currentNode.leftNode;
+					}
+				}
+				}
+			} while (!finished);
+		}
+		return returner;
+	}
 	public void traverseAndPrint(BstNode currentNode) {
 		System.out.print("data = " + currentNode.intData);
 		if (currentNode.leftNode == null) {
--- EclipseWorkspace/src/tester.java
+++ EclipseWorkspace/src/tester.java
@@ -8,5 +8,13 @@ public class tester {
 		bstObj.addNode(74);
 		bstObj.addNode(54);
 		bstObj.traverseAndPrint(bstObj.rootNode);
+		System.out.println(bstObj.Find(25));
+		System.out.println(bstObj.Find(52));
+		System.out.println(bstObj.Find(54));
+		System.out.println(bstObj.Find(56));
+		System.out.println(bstObj.Find(74));
+		System.out.println(bstObj.Find(4));//false
+		System.out.println(bstObj.Find(100));//false
+		System.out.println(bstObj.Find(53));//false
 	}
 }
